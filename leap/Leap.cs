9fca15d73d3377f1a5de931252eba1e62461ffe5
AndresAAV <andres.artaloytia@gmail.com>
2025-05-05 21:16:24 +0200

    Segunda iteracion de la solucion

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,17 +2,15 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        if(year%4 == 0 && year%100 ==0)
+        if(year%4 == 0)
         {
-            if(year%400 == 0)
+            if(year%100 == 0)
             {
-                return true;
-            }else
-                return false;
+                return year %400 == 0;
+            }
             
-        }else if(year%4== 0)
-        {
-            return true;
+            return true;            
+                     
         }
 
         return false;
