7930636fc736257f0c934d2ed2291f5006efd677
AndresAAV <andres.artaloytia@gmail.com>
2025-05-05 21:10:29 +0200

    Primera iteracion de la solucion

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,6 +2,19 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if(year%4 == 0 && year%100 ==0)
+        {
+            if(year%400 == 0)
+            {
+                return true;
+            }else
+                return false;
+            
+        }else if(year%4== 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
